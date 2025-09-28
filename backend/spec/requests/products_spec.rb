4b2278dc4bdae69b85a737ebc384a839348f8114
Hazatha <hazathaid@gmail.com>
2025-09-28 23:26:28 +0700

    test: add request spec for scraping product

---
+++ backend/spec/requests/products_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Products API", type: :request do
+  describe "POST /scrape" do
+    let(:url) { "https://www.flipkart.com/srpm-wayfarer-sunglasses/p/itmaf19ae5820c06" }
+
+    it "scrapes and saves a product" do
+      post "/scrape", params: { url: url }
+
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)["name"]).to be_present
+    end
+  end
+end
