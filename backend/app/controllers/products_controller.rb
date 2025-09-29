fe44fec72ec25e30c3eae7aac5ac0263ca29f3e3
Hazatha <hazathaid@gmail.com>
2025-09-30 00:38:51 +0700

    feat: add backend for get data from database

--- backend/app/controllers/products_controller.rb
+++ backend/app/controllers/products_controller.rb
@@ -1,4 +1,12 @@
 class ProductsController < ApplicationController
+  def index
+    products = Product.all.order(created_at: :desc)
+
+    render json: {
+      products: products.as_json(only: [:id, :name, :price, :description])
+    }
+  end
+
   def scrape
     url = params[:url]
     return render json: { error: "URL is required" }, status: :bad_request if url.blank?
